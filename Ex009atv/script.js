8bfc8a7339c8224ea01e844285c8f6bdbde5671f
leonardopereiraandrade <luisfelipemartinsbarbosa8@gmail.com>
2026-03-09 21:37:19 -0300

    Move Ex005, add Ex006 asset and new exercises
    Refactor: moved Ex004_listas/Ex005 files into top-level Ex005 directory (HTML, CSS and media). Add Ex006 asset (playstation-2 PNG) and remove outdated ps2.webp. Add new exercises: Ex009atv (index.html and script.js with greeting/alert logic) and Exx009 (panqueca recipe HTML and stylesheet).

---
+++ Ex009atv/script.js
@@ -0,0 +1,6 @@
+window.alert("Olá, seja bem-vindo ao curso de Desenvolvimento Web Frontend!");
+window.alert("Espero que você aproveite bastante o curso e aprenda muito!");
+
+let name = document.getElementById("saudacao").value;
+
+document.getElementById("saudacao").innerHTML = "Olá, " + name + "! Seja bem-vindo ao curso de Desenvolvimento Web Frontend!";
